4184e94bcb533b555eccd24674579948cbfe1177
yalincode <yukselyal@gmail.com>
2022-04-19 15:07:09 +0300

    Comment service development

---
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IBlogService
+    {
+        void BlogAdd(Blog category);
+        void BlogDelete(Blog category);
+        void BlogUpdate(Blog category);
+        List<Blog> GetList();
+        Blog GetById(int id);
+        List<Blog> GetBlogListWithCategory();
+        
+    }
+}
---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }
+        public void BlogAdd(Blog category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogDelete(Blog category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogUpdate(Blog category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetBlogListWithCategory()
+        {
+            return _blogDal.GetListWithCategory();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+
+        public List<Blog> GetBlogById(int id)
+        {
+            return _blogDal.GetListAll(X => X.BlogId == id);
+        }
+    }
+}
