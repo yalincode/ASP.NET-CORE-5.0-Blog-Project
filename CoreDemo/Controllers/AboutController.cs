b045afae9d7d95087f41f548511dd128be85ce0f
yalincode <yukselyal@gmail.com>
2022-04-30 11:34:13 +0300

    About Page

---
+++ CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+
+        public IActionResult Index()
+        {
+            var values = abm.GetList();
+            return View(values);
+        }
+
+        public PartialViewResult SocialMedialAbout()
+        {
+            
+            return PartialView();
+        }
+    }
+}
