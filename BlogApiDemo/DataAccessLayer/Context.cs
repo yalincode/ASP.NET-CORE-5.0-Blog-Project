229725cbba535cc67e9448fad1607f0815e33876
yalincode <yukselyal@gmail.com>
2022-05-06 21:21:22 +0300

    API crud operations (working part)

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-E7SV85T\\S2019;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApiDemo.DataAccessLayer
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
