58bb01052feb2e5f6a40b9765c0767621fbd1be1
yalincode <yukselyal@gmail.com>
2022-05-06 11:27:41 +0300

    Google Chart and AJAX

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,37 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list=new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryName="Teknoloji",
+                categoryCount=10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryName = "Yazılım",
+                categoryCount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                categoryName = "spor",
+                categoryCount = 5
+            });
+
+            return Json(new { jsonlist = list });
+        }
+    }
+}
--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -18,10 +18,10 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v2=c.Contacts.Count();
             ViewBag.v3=c.Comments.Count();
 
-            string api = "";//api yi buraya yaz
+            string api = "e83b3c4c08285bf87b99f9bbc0abe3f0";//api yi buraya yaz
             string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&units=metric&appid=" + api;
-            //XDocument document=XDocument.Load(connection);
-            //ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            XDocument document=XDocument.Load(connection);
+            ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
