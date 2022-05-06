92c8e0731c64b104ec5ff420438bc2adddff88e9
yalincode <yukselyal@gmail.com>
2022-05-06 14:57:54 +0300

    AJAX Crud operations in admin/writer/index

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -16,6 +17,39 @@ namespace CoreDemo.Areas.Admin.Controllers
         public IActionResult WriterList()
         {
             var jsonWriters=JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter=writers.FirstOrDefault(x=>x.Id== writerid);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer =writers.FirstOrDefault(x=>x.Id== id);
+            writers.Remove(writer);
+            var jsonWriters = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x=>x.Id== w.Id);
+            writer.Name = w.Name;
+            var jsonWriters = JsonConvert.SerializeObject(writer);
             return Json(jsonWriters);
         }
 
