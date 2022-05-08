af77b02e045c4d3ba0ffea2e3c7e87542263ebbd
yalincode <yukselyal@gmail.com>
2022-05-08 15:19:37 +0300

    Writer login with identity -Weiteraboutondashboard component (User.identity and Writer.Mail authentication)

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using DataAccessLayer.Concrate;
+﻿using CoreDemo.Models;
+using DataAccessLayer.Concrate;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,38 +13,64 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    [AllowAnonymous]//Tüm kısıtlamaları kaldırır.
     public class LoginController : Controller
     {
-        [AllowAnonymous]//Tüm kısıtlamaları kaldırır.
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        [AllowAnonymous]
         [HttpPost]
-        public async Task<IActionResult> Index(Writer p)
+        public async Task<IActionResult> Index(UserSignInViewModel p)
         {
-            Context c=new Context();
-            var dataValues=c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
-            if (dataValues != null)
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name,p.WriterMail)
-                };
-                var useridentity = new ClaimsIdentity(claims,"a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("index","Dashboard");
+                    return RedirectToAction("Index", "Dashboard");   
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Login");
+                }
             }
-            else
-            {
-                return View();
-            }
-
+            return View();
+            
         }
+
+        //[AllowAnonymous]
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer p)
+        //{
+        //    Context c=new Context();
+        //    var dataValues=c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+        //    if (dataValues != null)
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name,p.WriterMail)
+        //        };
+        //        var useridentity = new ClaimsIdentity(claims,"a");
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+        //        await HttpContext.SignInAsync(principal);
+        //        return RedirectToAction("index","Dashboard");
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+
+        //}//claim ile login
     }
 }
 //Context c = new Context();
--- CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrate;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreDemo.ViewComponents.Writer
 {
@@ -10,11 +13,12 @@ namespace CoreDemo.ViewComponents.Writer
     public class WriterAboutOnDashboard:ViewComponent
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
-
-        public IViewComponentResult Invoke()
+        Context c = new Context();
+        public  IViewComponentResult Invoke()
         {
-            var usermail = User.Identity.Name;
-            Context c = new Context();
+            var username = User.Identity.Name;
+            ViewBag.v= username;
+            var usermail=c.Users.Where(x=>x.UserName == username).Select(y=>y.Email).FirstOrDefault();
             var writerId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
             var values = wm.GetWriterById(writerId);
             return View(values);
