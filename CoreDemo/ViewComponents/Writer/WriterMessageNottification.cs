ffbb8deb4b7e7e4991270f4971403624e511551d
yalincode <yukselyal@gmail.com>
2022-05-09 17:25:58 +0300

    Admin page side bar routing operations and added message page

--- CoreDemo/ViewComponents/Writer/WriterMessageNottification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNottification.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrate;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.ViewComponents.Writer
 {
@@ -8,10 +10,13 @@ namespace CoreDemo.ViewComponents.Writer
 
     {
         Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c=new Context();
         public IViewComponentResult Invoke()
         {
-            int id = 2;
-            var values = mm.GetInboxListByWriter(id);
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerId);
             return View(values);
         }
 
