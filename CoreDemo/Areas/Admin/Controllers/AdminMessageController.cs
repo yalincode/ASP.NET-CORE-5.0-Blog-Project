486604163262eb848c855794e61474c53b3e1ee1
yalincode <yukselyal@gmail.com>
2022-05-12 10:44:41 +0300

    Admin messages pages (sendbox - inbox - compose mail)

--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
+
         public IActionResult Index()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerId);
+            return View(values);
+        }
+
+        public IActionResult SendBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterId).FirstOrDefault();
+            var values = mm.GetSendboxListByWriter(writerId);
+            return View(values);
+        }
+
+        public IActionResult ComposeMessage()
         {
             return View();
         }
